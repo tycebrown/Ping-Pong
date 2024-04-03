1f8915de90eec3c00c0a577ef2798018727e9a25
Ur16thMom <126522963+Ur16thMom@users.noreply.github.com>
2024-04-03 17:03:31 -0400

    Update letsdrawsomestuff.py

--- Scratch Stuff/letsdrawsomestuff.py
+++ Scratch Stuff/letsdrawsomestuff.py
@@ -11,10 +11,27 @@ def main():
     arcade.start_render()
 
     # ----------- Drawing code goes here
-
+    arcade.draw_circle_filled(400,250,200,arcade.color.YELLOW)
+    arcade.draw_line(300,125,500,125,arcade.color.BLACK,10)
+    arcade.draw_circle_filled(325,250,30,arcade.color.BLACK)
+    arcade.draw_circle_filled(475,250,30,arcade.color.BLACK)
+    arcade.draw_circle_outline(325,250,50,arcade.color.BALL_BLUE)
+    arcade.draw_circle_outline(475,250,50,arcade.color.BALL_BLUE)
+    arcade.draw_line(375,250,425,250,arcade.color.BALL_BLUE,3)
+    
     # We're done drawing
     arcade.finish_render()
 
     arcade.run()
 
 main()
+
+'''
+sudo code for basic clicker game:
+x=0
+window
+write x but big
+when space press x+=1
+clear screen
+rewrite x but big still
+'''
